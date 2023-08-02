117ede97ea563b0e5a590cf2ca5518475caeb3e4
Oanh-D <128280161+Oanh-D@users.noreply.github.com>
2023-08-02 14:59:26 -0700

    Add files via upload
    click on the body to play

---
+++ dicee.js
@@ -0,0 +1,72 @@
+
+var x = document.getElementsByClassName("container")[0];
+
+x.addEventListener("click", mainFunction);
+// alert();
+// x.addEventListener("click", draw);
+
+function mainFunction (){
+    var randomNumber1 = Math.floor(Math.random()*6) + 1;
+    // alert(randomNumber1);
+    switch (randomNumber1) {
+        case 1 :
+            document.getElementsByClassName("img1")[0].src = "/images/dice1.png";
+            break;
+        case 2 :
+            document.getElementsByClassName("img1")[0].src = "/images/dice2.png";
+            break;
+        case 3 :
+            document.getElementsByClassName("img1")[0].src = "/images/dice3.png";
+            break;
+        case 4 :
+            document.getElementsByClassName("img1")[0].src = "/images/dice4.png";
+            break;
+        case 5 :
+            document.getElementsByClassName("img1")[0].src = "/images/dice5.png";
+            break;
+        case 6:
+            document.getElementsByClassName("img1")[0].src = "/images/dice6.png";
+            break;
+    }
+
+    var randomNumber2 = Math.floor(Math.random()*6) + 1;
+    switch (randomNumber2) {
+        case 1 :
+            document.getElementsByClassName("img2")[0].src = "/images/dice1.png";
+            break;
+        case 2 :
+            document.getElementsByClassName("img2")[0].src = "/images/dice2.png";
+            break;
+        case 3 :
+            document.getElementsByClassName("img2")[0].src = "/images/dice3.png";
+            break;
+        case 4 :
+            document.getElementsByClassName("img2")[0].src = "/images/dice4.png";
+            break;
+        case 5 :
+            document.getElementsByClassName("img2")[0].src = "/images/dice5.png";
+            break;
+        case 6:
+            document.getElementsByClassName("img2")[0].src = "/images/dice6.png";
+            break;
+    }
+
+    randomNumber1 > randomNumber2 ? document.querySelector("h1").innerHTML = "🚩 Player 1 wins!" 
+        : randomNumber1 === randomNumber2 ? document.querySelector("h1").innerHTML = "Draw!" 
+        : document.querySelector("h1").innerHTML = "Player 2 wins! 🚩";
+
+    // if (randomNumber1 > randomNumber2){
+    //     document.querySelector("h1").innerHTML = "🚩 Player 1 wins!";
+    // }
+    // else if (randomNumber1 === randomNumber2){
+    //     document.querySelector("h1").innerHTML = "Draw!";
+    // }
+    // else {
+    //     document.querySelector("h1").innerHTML = "Player 2 wins! 🚩";
+    // }
+}
+
+// function draw (){
+//     document.querySelector("h1").innerHTML = "Draw!";
+// }
+
